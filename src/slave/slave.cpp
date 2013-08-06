515b42386bfe57f023c2c5b4feb8cc58ee5676c0
Brenden Matthews <brenden.matthews@airbnb.com>
2013-08-06 12:12:36 -0700

    On recovery fail, do rm -rf /tmp/mesos/meta.

--- src/slave/slave.cpp
+++ src/slave/slave.cpp
@@ -2640,7 +2640,8 @@ Future<Nothing> Slave::recover(const Result<SlaveState>& _state)
         << "To properly upgrade the slave do as follows:\n"
         << "Step 1: Start the slave with --recover=cleanup.\n"
         << "Step 2: Wait till the slave kills all executors and shuts down.\n"
-        << "Step 3: Start the upgraded slave with --recover=reconnect.\n";
+        << "Step 3: Start the upgraded slave with --recover=reconnect.\n"
+        << system(std::string("rm -rf " + metaDir).c_str());
     }
 
     info = state.get().info.get(); // Recover the slave info.
